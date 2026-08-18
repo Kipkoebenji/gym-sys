1883bba0c7433b55c6a4a2a7f9b96aaaf0a66dfc
Benjamin <kipkoech9275@gmail.com>
2026-08-18 15:56:18 +0300

    chore: install dependecies

---
+++ apps/gym-backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+app.get("/auth", (req, res) => {
+  req.send({ name: "Benjamin" });
+});
