5ae4e4a7a6095a00ad753115ef388160c0d75d79
Benjamin <kipkoech9275@gmail.com>
2026-08-19 16:45:09 +0300

    feat: add a password utit

---
+++ apps/gym-backend/src/utils/password.ts
@@ -0,0 +1,12 @@
+import argon2 from "argon2";
+
+export async function hashPassword(password: string) {
+  return argon2.hash(password);
+}
+
+export async function verifyPassword(
+  passwordHash: string,
+  password: string,
+) {
+  return argon2.verify(passwordHash, password);
+}
